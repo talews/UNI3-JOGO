d0354f41ee508e6a004a2bdda21f250c0d8dcb25
Your Name <tales.trab@gmail.com>
2017-08-14 01:32:34 -0300

    olosco

---
+++ Assets/scripts/cameraSeguir.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraSeguir : MonoBehaviour {
+
+    [SerializeField]
+    private float MaximoX;
+    [SerializeField]
+    private float MinX;
+    [SerializeField]
+    private float MaximoY;
+    [SerializeField]
+    private float MinY;
+
+    public Transform player;
+	// Use this for initialization
+	void Start () {
+        
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(Mathf.Clamp(player.position.x, MinX, MaximoX), Mathf.Clamp(player.position.y, MinY, MaximoY),transform.position.z);
+		
+	}
+}
---
+++ Assets/scripts/player.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour {
+
+    private Rigidbody2D rb20;
+     float horinzontal;
+    [SerializeField]
+    private float velocidade = 0;
+    private bool ladoreito;
+    private Animator animator;
+	void Start () {
+        rb20 = GetComponent<Rigidbody2D>();
+        ladoreito = transform.localScale.x > 0;
+        animator = GetComponent<Animator>();
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate() {
+      
+        horinzontal = Input.GetAxis("Horizontal");
+        Movimentar(horinzontal);
+        mudardirecao(horinzontal);
+
+
+    }
+    private void Movimentar(float h)
+    {
+        rb20.velocity = new Vector2(h * velocidade,rb20.velocity.y);
+        animator.SetFloat("velocidade", Mathf.Abs(h));
+        
+
+    }
+    private void mudardirecao(float horizontal)
+    {
+        if (horinzontal > 0 && !ladoreito || horinzontal < 0 && ladoreito)
+        {
+            ladoreito = !ladoreito;
+                transform.localScale = new Vector3(-transform.localScale.x,transform.localScale.y,transform.position.z);
+        }
+    }
+}
